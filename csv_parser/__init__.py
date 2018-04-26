92134e0133b059f1d7088c1aaa497ceba3ca3c46
Matus Moravcik <matus.moravcik@gmail.com>
2018-04-26 13:19:09 +0100

    Rename a header variable and add some comments

--- csv_parser/__init__.py
+++ csv_parser/__init__.py
@@ -14,21 +14,45 @@ class CSVParser:
         self.file = csv_file
 
     def _get_day_header_mapping(self, row):
+        """
+        For a row, lets create a week_day <-> header mapping dict so we
+        know which header to use for a certain day
+
+        Example output
+
+        ```
+            {
+                'tue': 'tue-wed',
+            },
+            {
+                'wed': 'tue-wed',
+            },
+            {
+                'mon': 'mon',
+            },
+
+            ....
+
+        :param row <array>
+        :return: <dict>
+        """
         day_header_mapping = {}
-        for column_name in row:
-            if column_name in self.week_days:
-                # In this case, column_name is the name of the day
-                day_header_mapping[column_name] = column_name
+
+        for header in row:
+            if header in self.week_days:
+                # In this case, `header` is the header for the day
+                day_header_mapping[header] = header
             else:
-                range_match = re.search(RANGE_REGEX, column_name)
+                # Lets see if we are dealing with the day range
+                range_match = re.search(RANGE_REGEX, header)
                 if range_match:
+                    # If we are, then start_day and end_day are our matches
                     start_day = range_match.group(1)
                     end_day = range_match.group(2)
                     start_day_index = self.week_days.index(start_day)
                     end_day_index = self.week_days.index(end_day)
-                    if start_day_index < end_day_index:
-                        for day in self.week_days[start_day_index:end_day_index + 1]:
-                            day_header_mapping[day] = column_name
+                    for day in self.week_days[start_day_index:end_day_index+1]:
+                        day_header_mapping[day] = header
         return day_header_mapping
 
     def _get_day_data(self, day, row, day_header_mapping):
