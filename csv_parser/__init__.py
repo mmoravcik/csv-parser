1fb12827bba56980e460b5e1f246d28e080aa906
Matus Moravcik <matus.moravcik@gmail.com>
2018-04-26 13:27:52 +0100

    Small day_data retrieval refactor and only consider 1 row in CSV

--- csv_parser/__init__.py
+++ csv_parser/__init__.py
@@ -56,34 +56,64 @@ class CSVParser:
         return day_header_mapping
 
     def _get_day_data(self, day, row, day_header_mapping):
-        value = int(row.get(day_header_mapping.get(day)))
-        description = row.get('description', '')
+        """
+        Constructs a dict which hold data about the specific day read from
+        the given row
 
-        day_data = dict(
-            day=day,
-            value=value,
-        )
+        Example output
 
-        # Lets workout the day specific data
-        if day in ['mon', 'tue', 'wed']:
-            day_data['square'] = value * value
-            day_data['description'] = "{} {}".format(
-                description, day_data['square']
+        ```
+            {
+                'day': 'mon',
+                'description: 'desc 1',
+                'value: 1,
+                'square': 1
+            },
+            ...
+        """
+        # Only consider the day if we have a data for it
+        if day_header_mapping.get(day):
+
+            # For now, assuming numbers only
+            value = int(row[day_header_mapping[day]])
+
+            # We could tweak this to make sure we handle no
+            # `description` column properly
+            description = row.get('description', 'N/A')
+
+            day_data = dict(
+                day=day,
+                value=value,
             )
-        elif day in ['thu', 'fri']:
-            day_data['double'] = value * 2
-            day_data['description'] = "{} {}".format(
-                description, day_data['double']
-            )
-        return day_data
+
+            # Lets workout the day specific data
+            if day in ['mon', 'tue', 'wed']:
+                day_data['square'] = value * value
+                day_data['description'] = "{} {}".format(
+                    description, day_data['square']
+                )
+            elif day in ['thu', 'fri']:
+                day_data['double'] = value * 2
+                day_data['description'] = "{} {}".format(
+                    description, day_data['double']
+                )
+            return day_data
 
     def parse(self):
         reader = csv.DictReader(self.file)
-        result = []
-        for row in reader:
-            day_header_mapping = self._get_day_header_mapping(row)
+        results = []  # This is where we hold the final list of day data
 
-            for day in self.week_days:
-                result.append(self._get_day_data(day, row, day_header_mapping))
+        # We are assuming only 1 row of data for now
+        row = next(reader)
 
-        return result
+        # Lets get the mapping so we know which day is which header
+        day_header_mapping = self._get_day_header_mapping(row)
+
+        # Lets get data for days, if present. This will also make
+        # sure the order of day data is correct
+        for day in self.week_days:
+            day_data = self._get_day_data(day, row, day_header_mapping)
+            if day_data:
+                results.append(day_data)
+
+        return results
